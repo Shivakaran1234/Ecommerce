1becd7c5cea15d5236bfa609b8a678850ca170be
Sharantej2348 <sharantej7623@gmail.com>
2026-02-02 16:11:39 +0530

    Initialize backend with Express, MongoDB, and Cloudinary configuration - Established connection to MongoDB and Cloudinary

---
+++ backend/config/mongodb.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const connectDB = async() => {
+
+    mongoose.connection.on('connected', () => {
+        console.log("DB Connected")
+    })
+
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/Ecommerce-MERN`)
+    } catch (error) {
+        console.log("MONGODB Connection Error: ", error)
+        process.exit(1)
+    }
+}
+
+export default connectDB;
